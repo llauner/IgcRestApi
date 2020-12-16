ab4e496bb1e674faae900f3fde34f6b7f9fc3f66
Ludovic Launer <ludovic.launer@atos.net>
2020-12-16 12:48:31 +0100

    Add CORS

--- IgcRestApi/Startup.cs
+++ IgcRestApi/Startup.cs
@@ -29,6 +29,15 @@ namespace IgcRestApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins("*");
+                    });
+            });
+
             services.AddControllers()
                 .AddNewtonsoftJson(options => JsonHelper.GetJsonSerializerSettings());
 
@@ -79,6 +88,7 @@ namespace IgcRestApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors();
             app.UseAuthentication();
             app.UseAuthorization();
 
